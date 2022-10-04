cdd6df8bd65e7b90ff639142d848190c0e92c95a
Ricardo Coelho <ricardocunhacoelho@gmail.com>
2022-10-04 20:48:36 -0300

    Faltando pouco arrumar no trabalho

--- lib/src/entrada/componentes/pagina.produtos.dart
+++ lib/src/entrada/componentes/pagina.produtos.dart
@@ -12,96 +12,106 @@ Widget paginaProdutos(String pagina, ConfiguracoesProdutoEstados produtostate,
           pagina.toUpperCase(),
           style: TextStyle(fontSize: 20),
         ),
-        SizedBox(height: 30),
+        SizedBox(height: 15),
         if (produtostate is CarregandoConfiguracoesProdutoEstados)
           Center(child: CircularProgressIndicator()),
         if (produtostate is CompletoConfiguracoesProdutoEstados)
           Expanded(
-            child: Scrollbar(
-              showTrackOnHover: true,
-              isAlwaysShown: true,
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: produtostate.lista.length,
-                itemBuilder: (context, index) {
-                  final produto = produtostate.lista[index];
-                  if (pagina.toLowerCase() == 'todos') {
-                    return ListTile(
-                      minLeadingWidth: 15,
-                      horizontalTitleGap: 18,
-                      contentPadding: EdgeInsets.all(10),
-                      trailing: Container(
-                        alignment: Alignment.centerRight,
-                        width: 150,
-                        child: ContadorQuantidadeComponente(produto: produto),
-                      ),
-                      leading: Image.asset(
-                        'assets/${produto.imagem}',
-                        height: 45,
-                        width: 45,
-                        fit: BoxFit.fitWidth,
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '${produto.descricao}',
-                            style: TextStyle(fontSize: 11),
-                          ),
-                        ],
-                      ),
-                      title: Text(
-                        '${produto.titulo} R\$${produto.preco}',
-                        style: TextStyle(fontSize: 13),
-                      ),
-                    );
-                  }
-                  if (produto.categoria.name == pagina.toLowerCase()) {
-                    return ListTile(
-                      minLeadingWidth: 15,
-                      horizontalTitleGap: 18,
-                      contentPadding: EdgeInsets.all(30),
-                      trailing: Container(
-                        alignment: Alignment.centerRight,
-                        width: 150,
-                        child: ContadorQuantidadeComponente(produto: produto),
-                      ),
-                      leading: Image.asset(
-                        'assets/${produto.imagem}',
-                        height: 45,
-                        width: 45,
-                        fit: BoxFit.fitWidth,
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '${produto.descricao}',
-                            style: TextStyle(fontSize: 11),
-                          ),
-                        ],
-                      ),
-                      title: Text(
-                        '${produto.titulo} R\$${produto.preco}',
-                        style: TextStyle(fontSize: 13),
-                      ),
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 100.0),
+              child: Scrollbar(
+                showTrackOnHover: true,
+                isAlwaysShown: true,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: produtostate.lista.length,
+                  itemBuilder: (context, index) {
+                    final produto = produtostate.lista[index];
+                    if (pagina.toLowerCase() == 'todos') {
+                      return ListTile(
+                        horizontalTitleGap: 18,
+                        contentPadding: EdgeInsets.all(5),
+                        trailing: Container(
+                          alignment: Alignment.centerRight,
+                          width: 150,
+                          child: ContadorQuantidadeComponente(produto: produto),
+                        ),
+                        leading: Image.asset(
+                          'assets/${produto.imagem}',
+                          height: 80,
+                          width: 80,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              '${produto.descricao}',
+                              style: TextStyle(fontSize: 11),
+                            ),
+                          ],
+                        ),
+                        title: Text(
+                          '${produto.titulo} R\$${produto.preco}',
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      );
+                    }
+                    if (produto.categoria.name == pagina.toLowerCase()) {
+                      return ListTile(
+                        minLeadingWidth: 15,
+                        horizontalTitleGap: 18,
+                        contentPadding: EdgeInsets.all(30),
+                        trailing: Container(
+                          alignment: Alignment.centerRight,
+                          width: 150,
+                          child: ContadorQuantidadeComponente(produto: produto),
+                        ),
+                        leading: Image.asset(
+                          'assets/${produto.imagem}',
+                          height: 80,
+                          width: 80,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              '${produto.descricao}',
+                              style: TextStyle(fontSize: 11),
+                            ),
+                          ],
+                        ),
+                        title: Text(
+                          '${produto.titulo} R\$${produto.preco}',
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      );
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
               ),
             ),
           ),
-        FloatingActionButton(
-          onPressed: () {},
-          child: Text('Finalizar'),
-        ),
+        // Padding(
+        //   padding: const EdgeInsets.only(bottom: 20.0, top: 15.0),
+        //   child: Container(
+        //     width: 80,
+        //     height: 80,
+        //     child: FloatingActionButton(
+        //       onPressed: () {},
+        //       child: Icon(Icons.check),
+        //     ),
+        //   ),
+        // ),
+        SizedBox(height: 15)
       ]),
     );
--- lib/src/entrada/pagina/entrada.web.responsivo.dart
+++ lib/src/entrada/pagina/entrada.web.responsivo.dart
@@ -1,5 +1,6 @@
 import 'package:fastfood_inteligente_flutter/src/configuracoes/bloc/configuracoes.produto.bloc.dart';
 import 'package:fastfood_inteligente_flutter/src/entrada/bloc/entrada.bloc.dart';
+import 'package:fastfood_inteligente_flutter/src/entrada/componentes/finalizarpedido.dialog.componente.dart';
 import 'package:fastfood_inteligente_flutter/src/entrada/componentes/pagina.produtos.dart';
 import 'package:fastfood_inteligente_flutter/src/entrada/estados/entrada.estados.dart';
 import 'package:fastfood_inteligente_flutter/src/entrada/modelo/entrada.ordem.model.dart';
@@ -26,11 +27,10 @@ class _EntradaWebResponsivoState extends State<EntradaWebResponsivo> {
       observacao: '',
       posicao: 1);
   double tamanho = 90;
-
+  bool habilitar = false;
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    bool habilitar = false;
     final IChecarSeFoiAdicionadoProduto checarSeFoiAdicionadoProduto =
         ChecarSeFoiAdicionadoProduto();
 
@@ -45,6 +45,9 @@ class _EntradaWebResponsivoState extends State<EntradaWebResponsivo> {
       ordemInicial = entradastates.lista.first;
     }
     final valor = checarSeFoiAdicionadoProduto.call(produtostate);
+    if (valor > 0) {
+      habilitar = true;
+    }
 
     final List<Widget> _screens = [
       paginaProdutos('lanche', produtostate, habilitar, ordemInicial),
@@ -91,6 +94,50 @@ class _EntradaWebResponsivoState extends State<EntradaWebResponsivo> {
             ],
           ),
           Expanded(child: _screens[_selectedIndex]),
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 30.0, bottom: 5, left: 20, right: 35),
+            child: Container(
+              margin: EdgeInsets.all(
+                15,
+              ),
+              width: 180,
+              height: 55,
+              decoration: BoxDecoration(
+                color: habilitar ? Colors.blue : Colors.black12,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    5,
+                  ),
+                ),
+              ),
+              child: TextButton(
+                style: TextButton.styleFrom(minimumSize: Size.infinite),
+                onPressed: habilitar
+                    ? () {
+                        showDialog(
+                            context: context,
+                            builder: (_) {
+                              return FinalizarPedidoDialogComponente(
+                                  ordemInicial);
+                            });
+                      }
+                    : null,
+                child: Text(
+                  "Finalizar",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
